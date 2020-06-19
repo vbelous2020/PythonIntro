b668f5c51a87d9ea06d96bece42582f18af25efa
Vladimir <vovabelous2012@gmail.com>
2020-06-19 21:02:20 +0300

    Homework8

--- Homework8/task1.py
+++ Homework8/task1.py
@@ -3,3 +3,20 @@
 возвращает строку, зашифрованную путем применения операции XOR (битовое исключающее ИЛИ) над символами строки с ключом.
 Написать также функцию `XOR_uncipher`, которая по зашифрованной строке и ключу восстанавливает исходную строку.
 """
+
+
+key = 'asdfln3j34tnonfdkjnflksdfnla'
+code = 'this is my message'
+
+
+def xor_cipher(s1, s2):
+    return "".join([chr(ord(c1) ^ ord(c2)) for (c1, c2) in zip(s1, s2)])
+
+
+encoded = xor_cipher(code, key)
+print(encoded)
+
+
+decoded = xor_cipher(encoded, key)
+print(decoded)
+
