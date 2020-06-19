1c77a8e32d99db6820bc20ddd775dd39d4734673
Vladimir <vovabelous2012@gmail.com>
2020-06-19 19:55:46 +0300

    Homework7

---
+++ Homework7/task.py
@@ -0,0 +1,29 @@
+"""
+Напишите программу на Python, которая на вход получает список списков:
+[
+[34587, 'Learning Python, Mark Lutz', 4, 40.95],
+[98762, 'Programming Python, Mark Lutz', 5, 56.80],
+[77226, 'Head First Python, Paul Barry', 3, 32.95],
+[88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]
+]
+и возвращает список кортежей. Каждый кортеж состоит из номера заказа и произведения
+цены на товары и количества. Стоимость товара должена быть увеличена на $10, если
+стоимость заказа меньше $100. Программа должна использовать lambda и map.
+"""
+
+lst = [
+    [34587, 'Learning Python, Mark Lutz', 4, 40.95],
+    [98762, 'Programming Python, Mark Lutz', 5, 56.80],
+    [77226, 'Head First Python, Paul Barry', 3, 32.95],
+    [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]
+]
+
+
+res = list(
+    map(
+        lambda line:
+        (line[0],
+            round(line[2] * line[3] + 10 if line[2] * line[3] < 100 else line[2] * line[3], 2)),lst
+        )
+)
+print(res)
