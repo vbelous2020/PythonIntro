d0efe65c57e53ee6eb16ecca16c1f4d8c4dd60c2
Vladimir <vovabelous2012@gmail.com>
2020-06-20 13:14:17 +0300

    Homework5(drawings not ended)

--- Homework5/task1 - drawings.py
+++ Homework5/task1 - drawings.py
@@ -1,21 +1,3 @@
-"""
-rows = 11
-cols = 11
-
-for i in range(rows):
-    for j in range(cols):
-        if i == cols or j == i or :
-            print('* ', end='')
-        else:
-            print('  ', end='')
-    print()
-
-print()
-
-#x == width / 2 or y == width / 2 - x or y == width / 2 + x
-#i == 0 || j == i || j == cols - i
-"""
-# x == width / 2 - y or x == width / 2 + y or y == width / 2 + x or x == (width - y) + width / 2
 
 height = 11
 width = 11
